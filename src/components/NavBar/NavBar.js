592551100f2969c82fd62871d4c672afcf4d22a9
Sasha Rodriguez Carlomagno <caballerosasha@hotmail.com>
2022-01-31 21:17:36 -0300

    navbar committed

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./NavBar.css";
+import logo from "../../../src/logo.png";
+
+const NavBar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <a href="#"><img src={logo}/></a>
+            </div>
+            <a href="#" className="item guitar"><p>Guitarras</p></a>
+            <a href="#" className="item bass"><p>Bajos</p></a>
+            <a href="#" className="item us"><p>Nosotros</p></a>
+            <a href="#" className="item contact"><p>Contacto</p></a>
+        </div>
+
+    )
+}
+
+export default NavBar;
